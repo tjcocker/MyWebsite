fba0d9fa2f843fdecb5120dc0796f482471f2ac9
tjcocker <tjcocker@yahoo.com>
2024-08-20 20:23:08 -0600

    added test folder, pyCharm

---
+++ More Tests/SeleniumWebDriver/websiteTest2.py
@@ -0,0 +1,24 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+ 
+class WebsiteTest(unittest.TestCase):
+ 
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+ 
+    def test_website(self):
+         
+        driver = self.driver
+
+        driver.get("https://tim-cockerham.com")
+ 
+        # assertion to confirm if title has name
+        self.assertIn("Tim Cockerham", driver.title)
+        self.assertIn("https://tim-cockerham.com/", driver.current_url)
+  
+    def tearDown(self):
+        self.driver.close()
+ 
+if __name__ == "__main__":
+    unittest.main()
---
+++ More Tests/k6/script.js
@@ -0,0 +1,34 @@
+import { browser } from 'k6/browser';
+import { check } from 'k6';
+
+export const options = {
+  scenarios: {
+    ui: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    },
+  },
+  thresholds: {
+    checks: ['rate==1.0'],
+  },
+};
+
+export default async function () {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  try {
+    await page.goto('https://tim-cockerham.com');
+
+    const header = await page.locator('h1').textContent();
+    check(header, {
+      header: (h) => h == 'Hi, welcome to my site',
+    });
+  } finally {
+    await page.close();
+  }
+}
