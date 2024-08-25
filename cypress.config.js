03faadf4206baadad6690869acd01d7c65d88c5a
tjcocker <tjcocker@yahoo.com>
2024-08-25 16:12:50 -0600

    updated cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://tim-cockerham.com',
     experimentalStudio: true,
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       
