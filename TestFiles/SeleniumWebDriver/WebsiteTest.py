62a2b2e9e0b2fd6a6158b32ebf294352bde2df7b
tjcocker <tjcocker@yahoo.com>
2024-09-02 16:00:23 -0600

    updates to images and index

--- TestFiles/SeleniumWebDriver/WebsiteTest.py
+++ TestFiles/SeleniumWebDriver/WebsiteTest.py
@@ -1,24 +1,23 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
- 
+
 class WebsiteTest(unittest.TestCase):
- 
+
     def setUp(self):
         self.driver = webdriver.Chrome()
- 
+
     def test_website(self):
-         
+
         driver = self.driver
 
         driver.get("https://tim-cockerham.com")
- 
-        # assertion to confirm if title has name
+
         self.assertIn("Tim Cockerham", driver.title)
         self.assertIn("https://tim-cockerham.com/", driver.current_url)
-  
+
     def tearDown(self):
         self.driver.close()
- 
+
 if __name__ == "__main__":
     unittest.main()
