2196543ad951773916cea51d046fac5a0e310882
tjcocker <tjcocker@yahoo.com>
2024-09-10 18:15:17 -0600

    updated WebsiteTest.py

--- TestFiles/SeleniumWebDriver/WebsiteTest.py
+++ TestFiles/SeleniumWebDriver/WebsiteTest.py
@@ -16,6 +16,9 @@ class WebsiteTest(unittest.TestCase):
         self.assertIn("Tim Cockerham", driver.title)
         self.assertIn("https://tim-cockerham.com/", driver.current_url)
 
+        element = self.driver.find_element_by_id('h1')
+        self.assertEqual(element.text, 'Welcome to my Website')
+                         
     def tearDown(self):
         self.driver.close()
 
