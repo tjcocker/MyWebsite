68fc7db3ace04a73a3039e4cc9868c9d2395b78c
tjcocker <tjcocker@yahoo.com>
2024-09-10 20:55:33 -0600

    index updates

--- TestFiles/SeleniumWebDriver/WebsiteTest.py
+++ TestFiles/SeleniumWebDriver/WebsiteTest.py
@@ -10,14 +10,9 @@ class WebsiteTest(unittest.TestCase):
     def test_website(self):
 
         driver = self.driver
-
         driver.get("https://tim-cockerham.com")
-
         self.assertIn("Tim Cockerham", driver.title)
         self.assertIn("https://tim-cockerham.com/", driver.current_url)
-
-        element = self.driver.find_element_by_id('h1')
-        self.assertEqual(element.text, 'Welcome to my Website')
                          
     def tearDown(self):
         self.driver.close()
