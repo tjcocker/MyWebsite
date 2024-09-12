33c11e989c2d48b1d1fbee324ce8585dfb314162
tjcocker <tjcocker@yahoo.com>
2024-09-11 18:33:12 -0600

    index and test updates

--- TestFiles/k6/script.js
+++ TestFiles/k6/script.js
@@ -26,7 +26,7 @@ export default async function () {
 
     const header = await page.locator('h1').textContent();
     check(header, {
-      header: (h) => h == 'Welcome to my Website',
+      header: (h) => h == 'Welcome',
     });
   } finally {
     await page.close();
--- tests/websitetest.spec.ts
+++ tests/websitetest.spec.ts
@@ -17,6 +17,6 @@ test('test', async ({ page }) => {
   await expect(page.getByRole('article')).toContainText('Milk');
   await page.getByRole('button', { name: 'clear items' }).click();
   await page.getByRole('link', { name: 'Return to Main Site' }).click();
-  await expect(page.locator('h1')).toContainText('Welcome to my Website');
+  await expect(page.locator('h1')).toContainText('Welcome');
   await expect(page.getByRole('strong')).toContainText('Email Me');
 });
