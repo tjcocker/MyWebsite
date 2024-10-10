44cac7c885a8b0bd833ee02749af046039d75408
tjcocker <tjcocker@yahoo.com>
2024-10-10 15:46:27 -0600

    test updates

--- cypress/e2e/webAppTest.cy.js
+++ cypress/e2e/webAppTest.cy.js
@@ -2,6 +2,7 @@
 
 describe('Web App Tests', () => {
   it('Page Navagation Tests', () => {
+    cy.wait(1000);
     cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/')
     cy.get('.flex-col > .__className_712214').should('have.text', 'Welcome  to my Next.js web app! This is a work in progress.');
     cy.get('span').click();
@@ -18,6 +19,7 @@ describe('Web App Tests', () => {
   })
 
   it('Edit Invoice Test', function () {
+    cy.wait(1000);
     cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/invoices');
     cy.get('.flex-grow > :nth-child(1) > :nth-child(1) > .__className_712214').should('have.text', 'Invoices');
     cy.get('.h-10 > .hidden').should('have.text', 'Create Invoice');
@@ -29,50 +31,53 @@ describe('Web App Tests', () => {
     cy.get('#amount').type('500');
     cy.get('#pending').check();
     cy.get('.bg-blue-500').should('have.text', 'Edit Invoice');
-    cy.get('.bg-blue-500').click('center');
-
+    cy.get('.bg-blue-500').click({ force: true });
     cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/invoices');
-      cy.get('.bg-white > :nth-child(1) > :nth-child(3)').should('have.text', '$500.00');
-    })
+    cy.get('.bg-white > :nth-child(1) > :nth-child(3)').should('have.text', '$500.00');
+  })
+ 
+  it('Delete Invoice Test', function () {
+    cy.wait(1000);
+    cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/invoices');
+    cy.get(':nth-child(1) > :nth-child(6) > .flex > form > .rounded-md > .w-5').click({ force: true });
+    cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/invoices');
+    cy.get('.bg-white > :nth-child(1) > :nth-child(3)').should('not.have.text', '$500.00');
+  })
 
-    it('Delete Invoice Test', function () {
-      cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/invoices');
-      cy.get(':nth-child(1) > :nth-child(6) > .flex > form > .rounded-md > .w-5').click();
-      cy.get('.bg-white > :nth-child(1) > :nth-child(3)').should('not.have.text', '$500.00');
-    })
+  it('Create Invoice Test', function () {
+    cy.wait(1000);
+    cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/invoices');
+    cy.get('.h-10 > .hidden').click();
+    cy.get('#customer').select('d6e15727-9fe1-4961-8c5b-ea44a9bd81aa');
+    cy.get('#amount').clear('1');
+    cy.get('#amount').type('19.88');
+    cy.get('#paid').check();
+    cy.get('.bg-blue-500').click({ force: true });
+    cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/invoices');
+    cy.get('.bg-white > :nth-child(1) > :nth-child(3)').should('have.text', '$19.88');
+  })
 
-    it('Create Invoice Test', function () {
-      cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/invoices');
-      cy.get('.h-10 > .hidden').click();
-      cy.get('#customer').select('d6e15727-9fe1-4961-8c5b-ea44a9bd81aa');
-      cy.get('#amount').clear('1');
-      cy.get('#amount').type('19.88');
-      cy.get('#paid').check();
-      cy.get('.bg-blue-500').click();
-      cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/invoices');
-      cy.get('.bg-white > :nth-child(1) > :nth-child(3)').should('have.text', '$19.88');
-    })
-
-    it('Invoice Search Test', function () {
-      cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/invoices');
-      cy.get('.-space-x-px > [href="/dashboard/invoices?page=2"]').click();
-      cy.get('.hidden > .bg-white > :nth-child(2) > :nth-child(2)').should('have.text', 'ant-man@tinyurl.net');
-      cy.get('.peer').clear('v');
-      cy.get('.peer').type('vi');
-      cy.get('.hidden > .bg-white > .w-full > :nth-child(2)').should('have.text', 'vision@jarvis.io');
-      cy.get('.peer').clear('v');
-      cy.get('.bg-white > :nth-child(4) > :nth-child(2)').should('have.text', 'hulk@smash.info');
-    })
-
-    it('Customer Search Test', function () {
-      cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/customers');
-      cy.get('.divide-y > :nth-child(1) > :nth-child(2)').should('have.text', 'ant-man@tinyurl.net');
-      cy.get('.peer').type('spi');
-      cy.get('.divide-y > :nth-child(1) > :nth-child(2)').should('have.text', 'tobey@friendly-neighborhood.org');
-      cy.get('.divide-y > :nth-child(1) > :nth-child(2)').should('not.have.text', 'ant-man@tinyurl.net');
-      cy.get('.peer').clear('spi');
-      cy.get('.divide-y > :nth-child(3) > :nth-child(2)').should('have.text', 'cap@us.gov');
-      cy.get('.divide-y > :nth-child(1) > :nth-child(2)').should('not.have.text', 'tobey@friendly-neighborhood.org');
+  it('Invoice Search Test', function () {
+    cy.wait(1000);
+    cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/invoices');
+    cy.get('.-space-x-px > [href="/dashboard/invoices?page=2"]').click();
+    cy.get('.hidden > .bg-white > :nth-child(2) > :nth-child(2)').should('have.text', 'ant-man@tinyurl.net');
+    cy.get('.peer').clear('v');
+    cy.get('.peer').type('vi');
+    cy.get('.hidden > .bg-white > .w-full > :nth-child(2)').should('have.text', 'vision@jarvis.io');
+    cy.get('.peer').clear('v');
+    cy.get('.bg-white > :nth-child(4) > :nth-child(2)').should('have.text', 'hulk@smash.info');
+  })
   
+  it('Customer Search Test', function () {
+    cy.wait(1000);
+    cy.visit('https://nextjs-dashboard-liard-mu-62.vercel.app/dashboard/customers');
+    cy.get('.divide-y > :nth-child(1) > :nth-child(2)').should('have.text', 'ant-man@tinyurl.net');
+    cy.get('.peer').type('spi');
+    cy.get('.divide-y > :nth-child(1) > :nth-child(2)').should('have.text', 'tobey@friendly-neighborhood.org');
+    cy.get('.divide-y > :nth-child(1) > :nth-child(2)').should('not.have.text', 'ant-man@tinyurl.net');
+    cy.get('.peer').clear('spi');
+    cy.get('.divide-y > :nth-child(3) > :nth-child(2)').should('have.text', 'cap@us.gov');
+    cy.get('.divide-y > :nth-child(1) > :nth-child(2)').should('not.have.text', 'tobey@friendly-neighborhood.org');
   })
 })
