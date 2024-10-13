b572de9b374c31508b9a0cc37dd82b16a0a96e84
tjcocker <tjcocker@yahoo.com>
2024-10-12 21:26:57 -0600

    pytest updates

---
+++ TestFiles/Other/SlotGame/tests/test_deposit.py
@@ -0,0 +1,13 @@
+from main import deposit
+
+def test_deposit() -> None:
+    amount = deposit()
+    assert amount == 100
+    
+def test_deposit_greater_than_zero() -> None:
+    amount = deposit()
+    assert amount > 0
+    
+def test_deposit_is_number() -> None:
+    amount = deposit()
+    assert type(amount) is int
