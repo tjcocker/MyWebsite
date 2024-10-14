bbcfbfe8bd46be754f4bbb4eb2469aed044d3e85
tjcocker <tjcocker@yahoo.com>
2024-10-14 13:16:26 -0600

    index updates

--- TestFiles/Other/SlotGame/tests/test_deposit.py
+++ TestFiles/Other/SlotGame/tests/test_deposit.py
@@ -1,30 +1,14 @@
 from main import deposit
 
+
 def test_deposit() -> None:
-    amount = deposit(100)
+    amount = deposit()
     assert amount == 100
     
 def test_deposit_greater_than_zero() -> None:
-    amount = deposit(100)
+    amount = deposit()
     assert amount > 0
     
 def test_deposit_is_number() -> None:
-    amount = deposit(100)
+    amount = deposit()
     assert type(amount) is int
-
-# @pytest.fixture 
-# def input_value(): 
-#    input = 100
-#    return input
-
-# def test_deposit() -> None:
-#     amount = input_value
-#     assert amount == 100
-    
-# def test_deposit_greater_than_zero() -> None:
-#     amount = input_value
-#     assert amount > 0
-    
-# def test_deposit_is_number() -> None:
-#     amount = input_value
-#     assert type(amount) is int
