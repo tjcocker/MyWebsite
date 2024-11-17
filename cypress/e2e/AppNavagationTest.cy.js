9071275a06b3a6962c3b2a25c6ddfa53e74f1935
tjcocker <tjcocker@yahoo.com>
2024-11-17 11:58:50 -0700

    test update

--- cypress/e2e/AppNavagationTest.cy.js
+++ cypress/e2e/AppNavagationTest.cy.js
@@ -4,6 +4,7 @@ context('Navagation', { tags: 'smoke' }, function () { // smoke tag
   describe('Page Navagation Tests', () => {
     it('Can navagate to every page', () => {
       cy.visit('/');
+      cy.wait(3000);
       cy.get('[data-cy=clickhere]').should('have.text', 'Click Here'); // using data attributes for selectors
       cy.getBySel("clickhere").click();         
       cy.title().should('eq', 'Tim Cockerham');                        // using custom commands for the selectors
