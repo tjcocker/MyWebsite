fe06cca7ddbb46b00b9c33372b44ec1273d83a7c
tjcocker <tjcocker@yahoo.com>
2024-11-17 14:22:49 -0700

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -19,5 +19,5 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-import registerCypressGrep from '@cypress/grep'
+const registerCypressGrep = require('@cypress/grep')
 registerCypressGrep()
