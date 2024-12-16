2f858c63bd5447cc4e5232e03c0620b082b5f2af
tjcocker <tjcocker@yahoo.com>
2024-12-16 07:43:35 -0700

    clean up and comments

--- tests/websitetest.spec.ts
+++ tests/websitetest.spec.ts
@@ -1,29 +1,14 @@
 import { test, expect } from '@playwright/test';
-
-test('Website Test', async ({ page }) => {
-  await page.goto('http://tim-cockerham.com/');
-  await expect(page.locator('h1')).toContainText('Welcome');
-  await expect(page.getByRole('img', { name: 'me and my dog Tony' })).toBeVisible();
-  await expect(page.locator('body')).toContainText('Testing');
-  await page.getByPlaceholder('Enter city name').click();
-  await page.getByPlaceholder('Enter city name').fill('Westminster');
-  await page.getByRole('button', { name: 'Get Weather' }).click();
-  await expect(page.locator('#city-name')).toContainText('Westminster');
-  await page.getByRole('link', { name: 'Grocery List' }).click();
-  await expect(page.getByRole('heading')).toContainText('grocery list');
-  await page.getByPlaceholder('e.g. eggs').click();
-  await page.getByPlaceholder('e.g. eggs').fill('Milk');
-  await page.getByRole('button', { name: 'submit' }).click();
-  await expect(page.getByRole('article')).toContainText('Milk');
-  await page.getByRole('button', { name: 'clear items' }).click();
-  await expect(page.getByRole('article')).toBeEmpty;
-  await page.getByRole('link', { name: 'Return to Main Site' }).click();
-  await expect(page.locator('h1')).toContainText('Welcome');
-  await page.goto('https://tim-cockerham.com/rockpaperscissors.html');
-  await expect(page.getByText('Round: 1 of')).toBeVisible();
-  await page.getByRole('button', { name: 'Rock' }).click();
-  await expect(page.getByText('Round: 1 of')).toBeVisible();
-  await page.getByRole('link', { name: 'Return to Main Site' }).click();
-  await page.getByRole('link', { name: 'Contact' }).click();
-});
-
+import { CityData } from '../tests/data/cities.json'
+  // Load citydata
+for (const [city, cities] of Object.entries(CityData)) {
+  // Interate through list of cities and assert on each
+  test(`Weather app test for ${cities}`, async ({ page }) => {
+    await page.goto('http://tim-cockerham.com/');
+    await expect(page.locator('h1')).toContainText('Welcome');
+    await page.getByPlaceholder('Enter city name').click();
+    await page.getByPlaceholder('Enter city name').fill(cities);
+    await page.getByRole('button', { name: 'Get Weather' }).click();
+    await expect(page.locator('#city-name')).toContainText(cities);
+  });
+}
